e18a62c0f80f572643e0c359ca51a01117e21c7c
pablo37 <claudio.iacovozzi@gmail.com>
2016-12-22 11:54:56 +0100

    no message

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,5 +1,6 @@
 package org.leanpoker.player;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
@@ -18,6 +19,29 @@ public class Player {
 
         int currentBuyIn = json.get("current_buy_in").getAsInt();
         int minimumRaise = json.get("minimum_raise").getAsInt();
+
+        JsonArray players = json.get("players").getAsJsonArray();
+
+        for (int i=0; i < players.size(); i++) {
+            JsonObject player = players.get(i).getAsJsonObject();
+            int id = player.get("id").getAsInt();
+            String name = player.get("name").getAsString();
+
+            System.out.println("**** " + id + " - " + name);
+
+            if (name == "Clanto") {
+                JsonArray hole_cards = player.get("hole_cards").getAsJsonArray();
+
+                for (j=0; j < hole_cards.size(); j++) {
+                    JsonObject currentCard = hole_cards.get(j).getAsJsonObject();
+                    String rank = currentCard.get("rank").getAsString();
+                    String suit = currentCard.get("suit").getAsString();
+
+                    System.out.println("**** current Card: " + rank + " - " + suit);
+
+                }
+            }
+        }
 
         return currentBuyIn+minimumRaise;
     }
